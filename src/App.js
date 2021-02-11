399d2f450a4d242972e10ff5221ecd529dce8a41
Diego Oliva <dienrique24@hotmail.com>
2021-02-11 17:05:15 -0600

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div class="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:py-12">h</div>
     </div>
   );
 }
