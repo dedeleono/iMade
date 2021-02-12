4b6cd372d25a806e33fcb091eca48fbec285a957
Diego Oliva <dienrique24@hotmail.com>
2021-02-11 19:00:33 -0600

    added router-dom and initialized it

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import './App.css';
 import Layout from './containers/Layout';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Layout />
+      <Layout>
+        <Switch>
+          <Route path component>Landing</Route>
+          <Route path component>404</Route>
+          <Route path component>About/Instagram</Route>
+          <Route path component>Contact</Route>
+          <Route path component>Shop</Route>
+        </Switch>
+      </Layout>
     </>
   );
 }
