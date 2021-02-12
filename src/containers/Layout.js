2deca23adad3645791ca27f6f978cf879f3f3bc6
Diego Oliva <dienrique24@hotmail.com>
2021-02-11 18:40:54 -0600

    created initial Layout

---
+++ src/containers/Layout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Layout extends Component {
+    render()
+    {
+        return(
+            <>
+                <header>Header</header>
+                <main></main>
+                <footer></footer>
+            </>
+        )
+    }
+}
+
+export default Layout;
