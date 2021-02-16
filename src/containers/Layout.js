d3e695734de2fa2218c429d03e4eac04c25d680f
Diego Oliva <dienrique24@hotmail.com>
2021-02-15 18:57:28 -0600

    created navbar

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -5,21 +5,23 @@ class Layout extends Component {
     {
         return(
             <>
-                <header>
-                    <div className="bg-white">
-                        <button className="bg-gray-800 text-gray-300">Button</button>
-                    </div>
-                </header>
-                <main>
-                    <div className="bg-gray-200 h-auto container-lg">center
-                    <ul>
-                        <li>p</li>
-                        <li>p</li>
-                        <li>p</li>
-                    </ul>
-                    </div>
-                </main>
-                <footer className="h-10 bg-blue-500"></footer>
+                <div className="flex flex-col h-screen min-h-screen">
+                    <header className="py-2 bg-gray-700 text-white text-center">
+                        <div className="bg-white">
+                            <button className="bg-blue-400 p-2 text-gray-300">Button</button>
+                        </div>
+                    </header>
+                    <main className="">
+                        <div className="bg-gray-200 flex-1 overflow-y-auto">center
+                        <ul>
+                            <li>p</li>
+                            <li>p</li>
+                            <li>p</li>
+                        </ul>
+                        </div>
+                    </main>
+                    <footer className="p-5 h-10 bg-blue-500 flex-none"></footer>
+                </div>
             </>
         )
     }
