8a041495c211a8334c1c9bc6e267a03adf15766e
Diego Oliva <dienrique24@hotmail.com>
2021-02-15 18:22:04 -0600

    added missing config for tailwind

--- src/containers/Layout.js
+++ src/containers/Layout.js
@@ -5,9 +5,21 @@ class Layout extends Component {
     {
         return(
             <>
-                <header className="bg-black">Header</header>
-                <main></main>
-                <footer></footer>
+                <header>
+                    <div className="bg-white">
+                        <button className="bg-gray-800 text-gray-300">Button</button>
+                    </div>
+                </header>
+                <main>
+                    <div className="bg-gray-200 h-auto container-lg">center
+                    <ul>
+                        <li>p</li>
+                        <li>p</li>
+                        <li>p</li>
+                    </ul>
+                    </div>
+                </main>
+                <footer className="h-10 bg-blue-500"></footer>
             </>
         )
     }
