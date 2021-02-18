7d6dfc0b220a0a83632fb125b0f9b74412d19f9c
Diego Oliva <dienrique24@hotmail.com>
2021-02-17 22:13:40 -0600

    completing navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,8 +2,16 @@ import NavItems from './Navigation_Items/NavItems'
 
 const Navbar = (props) => {
     return(
-        <header className="">
-            <NavItems />
+        <header className="flex flex-row justify-between">
+            <div>
+                a
+            </div>
+            <div className="">
+                <NavItems />
+            </div>
+            <div>
+                b
+            </div>
         </header>
     )
 }
