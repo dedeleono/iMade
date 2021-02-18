fd31135a6e85167a7b70509332325a8f577571b7
Diego Oliva <dienrique24@hotmail.com>
2021-02-18 16:48:57 -0600

    small changes to the router of the app

--- src/App.js
+++ src/App.js
@@ -5,18 +5,19 @@ import About from './components/About/About';
 import Contact from './components/Contact/Contact';
 import NotFound from './components/404/NotFound';
 import Landing from './components/Landing/Landing';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Layout>
         <Switch>
-          <Route component={NotFound}>404</Route>
+          {/*<Route path='/404' component={NotFound} />
+          <Redirect to="/404" />*/}
           <Route path="/landing" component={Landing}>Landing</Route>
           <Route path="/about" component={About}>About/Instagram</Route>
           <Route path="/contact" component={Contact}>Contact</Route>
-          <Route path="/" exact component={Shop}>Shop</Route>
+          <Route path="/" exact={true} component={Shop}>Shop</Route>
         </Switch>
       </Layout>
     </>
