97e2af725eb12320754d2861aee21a7d1630d8e9
Diego Oliva <dienrique24@hotmail.com>
2021-02-18 11:18:20 -0600

    style changes to tabs, won´t use navbar anymore

--- src/SlidingTabs.js
+++ src/SlidingTabs.js
@@ -9,20 +9,20 @@ function SlidingTabs({
   const [activeTab, setActiveTab] = useState(0)
   let finalClass = `${className} w-full flex sm:border-b sm:border-gray-300 relative flex-col sm:flex-row`
   let tabClassName =
-    " flex-1 sm:text-center font-medium pb-3 cursor-pointer hover:text-blue-400"
+    " flex-1 sm:text-center font-medium pb-3 cursor-pointer hover:text-red-400"
   let sliderClassName =
-    "hidden sm:block absolute bottom-0 left-0 h-1 bg-blue-400 transition-transform duration-300 ease-out"
-  const numTabs = tabs.length
-  if (numTabs === 1) sliderClassName += " w-full"
-  else if (numTabs === 2) sliderClassName += " w-1/2"
-  else if (numTabs === 3) sliderClassName += " w-1/3"
-  else if (numTabs === 4) sliderClassName += " w-1/4"
-  else if (numTabs === 5) sliderClassName += " w-1/5"
-  if (activeTab === 0) sliderClassName += " transform translate-x-0"
+    "hidden sm:block absolute bottom-0 left-0 h-1 bg-red-400 transition-transform duration-300 ease-out"
+  const numTabs = tabs.length;
+  if (numTabs === 1) sliderClassName += " w-full";
+  else if (numTabs === 2) sliderClassName += " w-1/2";
+  else if (numTabs === 3) sliderClassName += " w-1/3";
+/*  else if (numTabs === 4) sliderClassName += " w-1/4";
+  else if (numTabs === 5) sliderClassName += " w-1/5";*/
+  if (activeTab === 0) sliderClassName += " transform translate-x-0";
   else if (activeTab === 1) sliderClassName += " transform translate-x-full"
   else if (activeTab === 2) sliderClassName += " transform translate-x-double"
   else if (activeTab === 3) sliderClassName += " transform translate-x-triple"
-  else if (activeTab === 4) sliderClassName += " transform translate-x-quad"
+/*  else if (activeTab === 4) sliderClassName += " transform translate-x-quad"*/
   const AllTabs = tabs.map((t, index) => {
     const isObject = typeof t === "object"
     let name = isObject ? t.label : t
@@ -31,7 +31,7 @@ function SlidingTabs({
     return (
       <div
         key={`${value}-${index}`}
-        className={`${tabClassName} ${activeTab === index && "text-blue-400"}`}
+        className={`${tabClassName} ${activeTab === index && "text-red-400"}`}
         onClick={() => {
           setActiveTab(index)
           onChange(value)
