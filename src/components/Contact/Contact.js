0f4299c4b8d3cd0665e11d46de027a06b73bef66
Diego Oliva <dienrique24@hotmail.com>
2021-02-25 18:24:50 -0600

    .

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -9,19 +9,16 @@ const Contact = (props) => {
                 <div class="max-w-6xl mx-auto sm:px-6 lg:px-8">
                     <div class="mt-8 overflow-hidden">
                         <div class="grid grid-cols-1 md:grid-cols-2 sm:rounded-lg bg-white">
-                            <div class="p-6 mr-2 bg-red-500 sm:rounded-lg">
+                            <div class="p-6 mr-2 bg-red-500 sm:rounded-lg mx-1 my-1">
                                 <h1 class="text-4xl sm:text-5xl text-white font-extrabold tracking-tight">
                                     Get in touch
                                 </h1>
 
-                                <p class="text-normal text-lg sm:text-2xl font-medium text-gray-200 mt-2">
+                                <p class="text-normal text-lg sm:text-sm font-medium text-gray-200 mt-2">
                                     Fill in the form to start a conversation
                                 </p>
 
                                 <div class="flex items-center mt-4 text-gray-100 py-6">
-                                    {/*<svg fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z"/>
-                                        </svg>*/}
                                     <FontAwesomeIcon icon={faInstagram} size="3x" />
                                     <div class="ml-4 text-sm tracking-wide font-light w-40">
                                         @imade.gt
@@ -29,9 +26,6 @@ const Contact = (props) => {
                                 </div>
 
                                 <div class="flex items-center mt-2 text-gray-100">
-                                    {/*<svg fill="none" stroke="white" stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" viewBox="0 0 24 24" class="w-8 h-8 text-gray-500">
-                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"/>
-                                    </svg>*/}
                                     <FontAwesomeIcon icon="envelope" size="3x"/>
                                     <span class="ml-4 text-sm tracking-wide font-light w-40">
                                         i-made@gmail.com
