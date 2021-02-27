9f4f97c5df9c87e306b4d5fe8a5f714bf7573a21
Diego Oliva <dienrique24@hotmail.com>
2021-02-27 11:34:59 -0600

    landing comp init

--- src/components/Landing/Landing.js
+++ src/components/Landing/Landing.js
@@ -1,6 +1,36 @@
 const Landing = (props) => {
+
+    let slider = {
+        overflow: "hidden",
+    }
+
+    let figure = {
+        position: "relative",
+        width: "auto",
+        margin: "0",
+        left: "0",
+        animation: "20s slider infinte"
+    }
+
+    let img = {
+        width: "20%",
+        float: "left"
+    }
+
     return(
-        <div>Landing</div>
+        <>
+            <div className="container w-full h-2/3 my-10 flex flex-row z-10 rounded-3xl shadow-md">
+                <div className={`flex flex-1 rounded-3xl ${slider}`}>
+                    <div className={`${figure}`}>
+                        <img alt="" className={`${img}`}>lel</img>
+                        <img alt="" className={`${img}`}>lil</img>
+                        <img alt="" className={`${img}`}>lol</img>
+                        <img alt="" className={`${img}`}>lul</img>
+                    </div>
+                </div>
+                <div className="flex flex-1 px-10">Logo</div>
+            </div>
+        </>
     )
 }
 
