d2efd5b79cdd60cf34a7db4eb9633d34634136de
Diego Oliva <dienrique24@hotmail.com>
2021-02-27 17:02:27 -0600

    inor style changes

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,7 +4,7 @@ const About = (props) => {
     return(
         <>
             <div className="container w-full h-2/3 my-10 flex flex-row z-10 rounded-3xl shadow-md">
-                <div className="flex flex-1 rounded-3xl"><img className="object-fill" alt="" src={img}/></div>
+                <div className="flex flex-1 rounded-3xl"><img className="object-fill rounded-3xl" alt="" src={img}/></div>
                 <div className="flex flex-1 px-10">
                     About us bla bla bla
                 </div>
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,12 +1,14 @@
 import Tabs from './Navigation_Items/Tabs'
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
 import { faShoppingCart } from '@fortawesome/free-solid-svg-icons';
+import logo from '../../Images/logo.jpeg'
+import {Link} from 'react-router-dom'
 
 const Navbar = (props) => {
     return(
         <div className="flex flex-row justify-between">
-            <div className="w-4 px-15 py-5">
-                    a
+            <div className="w-4 px-15 py-5 container flex w-28">
+                <Link to="/landing"><img src={logo} alt="" className="mx-3 object-fill"></img></Link>
             </div>
             <div className="container ">
                     {/*<NavItems /> <SlidingTabs/>*/}
